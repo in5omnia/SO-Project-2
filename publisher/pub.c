68ef996a8b8a1385638b03a7337527b1e8bfa99e
in5omnia <93283175+in5omnia@users.noreply.github.com>
2023-01-15 19:48:40 +0000

    COmmito puwese

--- publisher/pub.c
+++ publisher/pub.c
@@ -12,40 +12,35 @@
 int client_pipe;
 char client_pipe_name[MAX_CLIENTPIPE_NAME];
 
-void exit_process(){
+void exit_process() {
 	close(client_pipe);
 	unlink(client_pipe_name);
 	exit(0);
 }
 
 char *get_message() {
-	char *message = (char*)malloc(sizeof(char)*MAX_MESSAGE_SIZE);
-	//FIXME vai dar erro se a message for grande demais - tenho de a truncar e por o char 256 a \0 INTERNAL
-	size_t bytes_read = fread(message, sizeof(char), MAX_MESSAGE_SIZE, stdin);
-
-	if (bytes_read == MAX_MESSAGE_SIZE) {
+	char *message = (char *) malloc(sizeof(char) * MAX_MESSAGE_SIZE);
+	if (fgets(message,MAX_MESSAGE_SIZE, stdin) == NULL) {
 		if (feof(stdin)) {
 			exit_process();
 		}
 		PANIC("Failed to read from stdin");
-		return NULL;
 	}
-
 
 	size_t len = strlen(message);
-	if (message[len-1] == '\n') {	//replace newline
-		message[len-1] = '\0';
+	if (message[len - 1] == '\n') {    //replace newline
+		message[len - 1] = '\0';
 	}
 	if (strlen(message) < MAX_MESSAGE_SIZE) {
-		memset(message + len, '\0', MAX_MESSAGE_SIZE - len);	//FIXME check for error
-		// fills the rest of the message with '\0'§
+		memset(message + len, '\0', MAX_MESSAGE_SIZE - len);    //FIXME check for error
+		// fills the rest of the message with '\0'
 	}
 	return message;
 }
 
 
 int publish_message(int client_pipe_fd, char *message) {
-	message_t *msg = (message_t*)malloc(sizeof(message_t));
+	message_t *msg = (message_t *) malloc(sizeof(message_t));
 	msg->code = CODE_PUBLISHER_SEND_MSG;
 	memcpy(msg->message, message, MAX_MESSAGE_SIZE);
 	ssize_t byteswritten = write(client_pipe_fd, msg, MAX_MESSAGE_SIZE);
@@ -63,7 +58,7 @@ int read_input(char *buffer, char *client_pipe_namepath, char *box_name) {
 	char input[MAX_INPUT_SIZE];
 	char assert[MAX_INPUT_SIZE];
 	if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
-		if (feof(stdin)){
+		if (feof(stdin)) {
 			exit_process();
 		}
 		PANIC("Failed to read from stdin");
@@ -76,7 +71,8 @@ int read_input(char *buffer, char *client_pipe_namepath, char *box_name) {
 	}
 	size_t client_pipe_len = strlen(client_pipe_namepath);
 	size_t box_name_len = strlen(box_name);
-	memset(client_pipe_namepath + client_pipe_len, '\0', MAX_CLIENTPIPE_NAME - client_pipe_len);    //FIXME check for error
+	memset(client_pipe_namepath + client_pipe_len, '\0',
+		   MAX_CLIENTPIPE_NAME - client_pipe_len);    //FIXME check for error
 	memset(box_name + box_name_len, '\0', MAX_BOX_NAME - box_name_len);    //FIXME check for error
 	// fills the names name with '\0'
 	return 0;
@@ -112,9 +108,9 @@ int main(int argc, char **argv) {
 		PANIC("Failed to send request to server");
 	}
 	free(register_pipe_name);
-	client_pipe = start_fifo(client_pipe_name, O_WRONLY);//FIXME check if it worked?
-	//threads sending messages?
+	client_pipe = start_fifo(client_pipe_name, O_WRONLY);
 	while (1) {
+		// Read
 		publish_message(client_pipe, get_message());
 	}
 	return 0;
